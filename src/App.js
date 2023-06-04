ac5a2936350be087adb78198fc6e0a643b4a326f
rizkyperdananst <riskiperdana345@gmail.com>
2023-06-05 02:12:37 +0700

    Practice First Make Card Login Using React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './profile.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <div className="card-header">
+          <img src={Profile} alt="Profile"/>
+          <h2>Input Your Credentials</h2>
+        </div>
+        <div className="card-body">
+          <div className='form-group'>
+            <input type="email" minLength={5} required placeholder="input your email"/>
+          </div>
+          <div className='form-group'>
+            <input type="password" minLength={5} required placeholder="input your password"/>
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-primary'>login</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
